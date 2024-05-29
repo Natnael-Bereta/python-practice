a86b4091d6f7cc6307b253862fe66a56c5116beb
Natnael-Bereta <natnaelbereta@gmail.com>
2024-05-29 19:51:37 +0300

    new file

---
+++ Random problems/sum_of_the_first_n_numbers.py
@@ -0,0 +1,10 @@
+try:
+    number = int(input("Insert any number: "))
+    if number < 0:
+        raise ValueError("Input should be a non-negative integer.")
+    total = 0
+    for i in range(1, number + 1):
+        total += i
+    print(f'The sum is = {total}')
+except:
+    print("You can only insert positive integers!")
