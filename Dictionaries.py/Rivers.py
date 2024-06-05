8662c61ab0f00b6a6e68b19a48dcac199f78cb1e
Natnael-Bereta <natnaelbereta@gmail.com>
2024-06-05 09:53:10 +0300

    new ones

---
+++ Dictionaries.py/Rivers.py
@@ -0,0 +1,4 @@
+river_name = {'Nile': 'Egypt',
+              'Awash': 'Ethiopia',
+              'Amazon': 'South America',
+              'Missisipi river' : 'North America'}
--- Dictionaries.py/favorite_numbers.py
+++ Dictionaries.py/favorite_numbers.py
@@ -1,5 +1,9 @@
 name_number = {'Natty': 23,
                'Beki': 42,
                'Kiya': 32,
-               'Lemi': 57}
-print(f'Natty: {name_number["Natty"]} \nBeki: {name_number["Beki"]} \nKiya: {name_number["Kiya"]} \nLemi: {name_number["Lemi"]}')
+               'Lemi': 57,
+               'Umeran': 49,
+               'Sifen': 38}
+#print(f'Natty: {name_number["Natty"]} \nBeki: {name_number["Beki"]} \nKiya: {name_number["Kiya"]} \nLemi: {name_number["Lemi"]}')
+for name, number in name_number.items():
+    print(f'{name}: {number}')
