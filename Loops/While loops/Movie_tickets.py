dcc667a87594884bacfaca1550f02f3bf9a90930
Natnael-Bereta <natnaelbereta@gmail.com>
2024-06-07 21:20:56 +0300

    new modified

--- Loops/While loops/Movie_tickets.py
+++ Loops/While loops/Movie_tickets.py
@@ -3,12 +3,16 @@ while True:
     user_input = input("How old are you? ")
     if user_input == 'quit':
         print("Exiting the program.")
-    age = int(user_input)
-    if age <= 3:
-        print("The ticket is free!")
-    elif 3 < age <= 12:
-        print("$10 for the ticket.")
-    elif age < 0:
-        print("Age can not be negative!")
-    else:
-        print("$15 for the ticket.")
+        break
+    try:
+        age = int(user_input)
+        if age <= 3:
+            print("The ticket is free!")
+        elif 3 < age <= 12:
+            print("$10 for the ticket.")
+        elif age < 0:
+            print("Age can not be negative!")
+        else:
+            print("$15 for the ticket.")
+    except ValueError:
+        print("You can not insert a string except 'quit'!")
