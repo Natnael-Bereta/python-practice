3432ecc0c666bd5505fe4d5fb25b1c0d248b5de5
Natnael-Bereta <natnaelbereta@gmail.com>
2024-06-09 15:14:29 +0300

    new

---
+++ Functions/T-shirt.py
@@ -0,0 +1,8 @@
+print('Order your T-shirt by providing your size and what to write on it!')
+size_input = input('Insert your size: ')
+word_input = input('What do you like to be wrote on your T-shit: ')
+
+def t_shirt(size, words):
+    print("\nYour T-shirt's size: "+ str(size) + '\nWhat to write on: ' + str(words))
+
+t_shirt(size= size_input, words= word_input)
