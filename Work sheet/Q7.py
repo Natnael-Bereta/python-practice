9e2e62746755ff730b464f05bd32f8708677618c
Natnael-Bereta <natnaelbereta@gmail.com>
2024-06-11 00:56:27 +0300

    new files

--- Work sheet/Q7.py
+++ Work sheet/Q7.py
@@ -1,10 +1,7 @@
 string = input("Insert a word: ")
-list_of_strings = []
-for laters in string:
-    new = list_of_strings.append(laters)
-    initial_2 = new[:2]
-    final_2_laters = new[-3:]
-    if len(string) <= 2:
-        print(string + string)
-    else:
-        print(initial_2 + final_2_laters)
+if len(string) <= 2:
+    print(string + string)
+else:
+    initial_2 = string[:2]
+    final_2 = string[-2:]
+    print(initial_2 + final_2)
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-str1 = 'Hello'
-str2 = 'World'
-str1 += str2
-print(len(str1))
-print ("Helloworld" in str1)
-print (str1.split())
+string = input("Insert a word: ")
+if len(string) <= 2:
+    print(string + string)
+else:
+    initial_2 = string[:2]
+    final_2 = string[-2:]
+    print(initial_2 + final_2)
