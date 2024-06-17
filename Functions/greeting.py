224a0a62b246144e88ba7eae1a4812bfd97c2662
Natnael-Bereta <natnaelbereta@gmail.com>
2024-06-17 21:15:47 +0300

    new

---
+++ Functions/greeting.py
@@ -0,0 +1,7 @@
+#Passing a List
+def greeting(names):
+    for name in names:
+        print(f'Hellow {name}')
+
+user_name = ['Natty', 'Beki', 'Lemi']
+greeting(user_name)
--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-string = input("Insert a word: ")
-if len(string) <= 2:
-    print(string + string)
-else:
-    initial_2 = string[:2]
-    final_2 = string[-2:]
-    print(initial_2 + final_2)
+def greet_user(user_name):
+    """Display a simple greeting."""
+    print("Hello " + user_name)
+greet_user("Natty")
+    
