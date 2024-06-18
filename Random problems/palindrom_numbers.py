05d12c6cb9d37bcf659ad7078e69f14dd2f176de
Natnael-Bereta <natnaelbereta@gmail.com>
2024-06-19 00:44:57 +0300

    new

--- Random problems/palindrom_numbers.py
+++ Random problems/palindrom_numbers.py
@@ -2,11 +2,11 @@
 # Input = int(input(">  "))
 Input = int(input("Insert a number to check whether it is a palindrome or not. \n >"))
 numbers = str(Input)
-inverted = ''
+reverted = ''
 for number in numbers:
-    inverted = number + inverted
+    reverted = number + reverted
 
-if inverted == numbers:
+if reverted == numbers:
     print("It's a palindrom!")
 else:
     print("It's not a palindrome!")
