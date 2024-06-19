4ebc078c3765358bd4f67e7fe47b65d3edf985e3
Natnael-Bereta <natnaelbereta@gmail.com>
2024-06-19 23:37:02 +0300

    new

--- Loops/pyramids/diamond.py
+++ Loops/pyramids/diamond.py
@@ -1,1 +1,19 @@
 n = 5
+for i in range(1, n+1):
+    for j in range(n-i):
+        print(' ', end= '')
+    for k in range(i):
+        print('* ', end='')
+    print()
+
+for p in range(n-1, 0, -1):
+    for s in range(n-p):
+        print(" ", end='')
+    for y in range(p):
+        print("* ", end='')
+    print()
+"""    print(' '* (n-i), end=' ')
+    print("* " * (i))
+for k in range(n-1, 0, -1):
+    print(' ' * ((n-1)-2), end=' ')
+    print("* " * (n-2))"""
